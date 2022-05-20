38d4284d9fd215f788d5e40126b0623d00edcee5
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-05-20 17:52:15 -0300

    funcao de mandar mensagem

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+require('dotenv').config();
 
+const token = process.env.DISCORD_TOKEN;
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -12,3 +13,12 @@ client.once('ready', () => {
 
 // Login to Discord with your client's token
 client.login(token);
+
+let canal = getCanal();
+async function getCanal() {
+    let message = 'Abiduda'
+    servidor = await client.guilds.fetch('744336301041516565');
+    await servidor.channels.fetch('791085718616408094').then(channel => channel.send(message)).catch(console.error);
+    return;
+}
+
